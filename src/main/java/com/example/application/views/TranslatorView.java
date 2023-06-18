ecedb7c8bf2e958738f9386b4cffb52de58aca09
dasu <kirill12093@mail.ru>
2023-06-19 01:45:49 +0300

    fix translator

--- src/main/java/com/example/application/views/TranslatorView.java
+++ src/main/java/com/example/application/views/TranslatorView.java
@@ -6,18 +6,21 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.upload.MultiFileReceiver;
+import com.vaadin.flow.component.upload.Receiver;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.Route;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -39,6 +42,7 @@ public class TranslatorView extends VerticalLayout {
         setUpInputArea();
         createTranslateButton();
         setUpOutputArea();
+        setUploadFileArea();
     }
 
     private void setUpInputArea() {
@@ -46,6 +50,42 @@ public class TranslatorView extends VerticalLayout {
         input.setWidthFull();
         input.setLabel("Text to translate");
         add(input);
+    }
+
+    private void setUploadFileArea() {
+        MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
+        Upload uploadField = new Upload(buffer);
+        uploadField.setMaxFiles(100);
+        uploadField.setMaxFileSize(1 * 1024 * 1024);
+        uploadField.setDropLabel(new Label("Drop file here (max 1MB)"));
+
+        Span errorField = new Span();
+        errorField.setVisible(false);
+        errorField.getStyle().set("color", "red");
+
+        uploadField.addFailedListener(e -> showErrorMessage(e.getReason().getMessage(), errorField));
+        uploadField.addFileRejectedListener(e -> showErrorMessage(e.getErrorMessage(), errorField));
+        uploadField.addSucceededListener(event -> {
+            try {
+                String fileName = event.getFileName();
+                InputStream inputStream = buffer.getInputStream(fileName);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + System.lineSeparator());
+                }
+                input.setValue(sb.toString());
+                output.setValue(translator.translate(sb.toString()));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                createError(e1.getMessage());
+            }
+        });
+
+        add(uploadField, errorField);
     }
 
     private void createTranslateButton() {
@@ -56,21 +96,22 @@ public class TranslatorView extends VerticalLayout {
                 if (translator.getDictionary() == null) {
                     throw new FileReadException("Dictionary didn't load, sorry.");
                 }
-                File tmp = File.createTempFile("tmp", "txt", Path.of("src/main/resources").toFile());
-                try (FileWriter fw = new FileWriter(tmp);
-                     BufferedWriter bw = new BufferedWriter(fw))
-                {
-                    bw.write(input.getValue());
+                if (input.getValue().isEmpty()) {
+                    throw new FileReadException("Input is empty, write something.");
                 }
-                String text = readFile(tmp.toString(), StandardCharsets.UTF_8);
-                output.setValue(translator.translate(text));
-                tmp.deleteOnExit();
-            } catch (FileReadException | IOException e) {
+            } catch (FileReadException e) {
                 createError(e.getMessage());
             }
         });
         add(button);
     }
+
+
+    private void showErrorMessage(String message, Span errorField) {
+        errorField.setVisible(true);
+        errorField.setText(message);
+    }
+
 
     private void setUpOutputArea() {
         output = new TextArea();
